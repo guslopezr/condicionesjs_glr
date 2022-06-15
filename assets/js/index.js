494b0e148ce2de19467ac68a7c5c5f4d4203334c
guslopezr <guslopezr@gmail.com>
2022-06-15 13:12:31 -0400

    se agrega segundo desafío

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,32 @@
-function myFunction(element, color) {
-    border = document.getElementById('image');
-    border.style.borderColor = 'transparent';
+/* Solución borde de color */
+
+/*
+let border = document.getElementById("image");
+let noborder = 0;
+*/
+
+
+border = 1;
+console.log(border);
+
+function cambiarBorde() {
+    if (border == 1) {
+        document.getElementById('image').style.border = '5px solid red';
+        border = 0;
+
+    } else {
+        document.getElementById('image').style.border = 'none';
+        border = 1;
+    }
+
 }
+
+
+/*
+function myFunction(element, color) {
+    document.getElementById('image').border.style.borderColor = 'red';
+
+
+}
+
+*/
